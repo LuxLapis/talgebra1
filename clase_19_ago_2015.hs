169e46cd98cb0f6df37dc67926338c1e43bd449f
LuxLapis <gabriel.lichtenstein@gmail.com>
2015-08-27 14:10:06 -0300

    Update clase_19_ago_2015.hs

--- clase_19_ago_2015.hs
+++ clase_19_ago_2015.hs
@@ -1,4 +1,3 @@
-
 --calcular el doble de un numero
 doble x = 2 * x
 
@@ -51,23 +50,24 @@ funcionDeTresNs n1 n2 n3
     | n2 >= 10 = n1 + n3
 
 --funcion nand (x,y) = ¬ (x ^ y)  | && = and, || = or
-nand_equals_nor _ _ = not ( _ && _ ) == not ( _ || _ )
-
-not (2<4) && not (3<6) == not (2<4) || not (3<6)
+nand_equals_nor x y = not ( x && y ) == not ( x || y )
 
 --este funciona: 
-not ((2<4) && (3<6)) == not ((2<4) || (3<6))
+-- not ((2<4) && (3<6)) == not ((2<4) || (3<6))
 
 --funcion nor (x,y) = ¬ (x v y)
-nor_equals_nand _ _ = not ( _ || _ ) == not ( _ && _ )
+nor_equals_nand x y = not ( x || y ) == not ( x && y )
 
 -- a,b,c pertenece a lo numeros reales. Calcular alguna de las raices de la funcion cuadratica
 -- f (x) = ax^2 + bx^2 + c
 
-raiz_negativa a b c = ( (-b) + ( sqrt ( (b^2)  - (4 * a * c) ) ) / (2 * a) )
-raiz_positiva a b c = ( (-b) - ( sqrt ( (b^2)  - (4 * a * c) ) ) / (2 * a) )
+raiz_negativa a b c = ( -(b) + ( sqrt ( (b^2)  - (4 * a * c) ) ) / (2 * a) )
+raiz_positiva a b c = ( -(b) - ( sqrt ( (b^2)  - (4 * a * c) ) ) / (2 * a) )
     
 -- es pitagorica con tres parametros a,b,c y diga si existe un triangulo rectangulo donde a y b sean las medidas de los
--- catetos y c la de la hipotenusa.
+-- catetos y c la de la hipotenusa. por ejemplo 3, 4 y 5
 
 esPitagorica a b c = (c^2) == (a^2) + (b^2)
+
+-- calcular la hipotenusa de un triangulo:
+hipotenusa a b = sqrt ( (a^2) + (b^2) ) 
